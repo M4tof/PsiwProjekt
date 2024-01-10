42b383674dff5a29e9335b1f4e21034045f72c85
krzys <krzysztfmanczak@gmail.com>
2024-01-10 17:54:00 +0100

    3

---
+++ testR.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <time.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+int main(int argc, char* argv[]) {
+    char klucz[strlen(argv[1])];
+    strcpy(klucz,argv[1]);
+
+    int pdesk = open(klucz,O_WRONLY);
+    if (pdesk == -1){
+        perror("Opening FIFO\n");
+        return 1;
+    }
+    
+    //main loop
+    while(1){
+        int data[3];
+        read(pdesk,data,3);
+        printf("Zamówienie z %d A, %d B, %d C\n",data[0],data[1],data[2]);
+    }
+
+    return 0;
+}
---
+++ testS.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <time.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+#define sleep_time 0.5
+
+int main(int argc, char* argv[]){
+    srand(time(NULL));
+    
+    char klucz[strlen(argv[1])];
+    strcpy(klucz,argv[1]);
+    
+    if (mkfifo(klucz,0600) == -1){
+        perror("Making FIFO\n");
+        return 1;
+    }
+    int A, B, C;
+    
+    //main loop
+    int pdesk = open(klucz,O_WRONLY);
+    if (pdesk == -1){
+        perror("Opening FIFO\n");
+        return 1;
+    }
+
+    int i=0;
+    while(1){
+        sleep(sleep_time);
+        A = rand() % 2;
+        B = rand() % 4;
+        C = rand() % 5;
+        printf("Zamowienie %d -> A:%d , B:%d , C:%d \n",i++,A,B,C);
+        int message[3];
+        message[0] = A;
+        message[1] = B;
+        message[2] = C;
+        write(pdesk,message,3); 
+    }
+
+    unlink(klucz);
+    return 0;
+}
